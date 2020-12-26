1471ad547ae7cf920227721772a38871c49faec2
Lilia16012001 <ivanskalilia16@gmail.com>
2020-12-27 00:57:01 +0200

    Column editing example

--- src/main/java/com/dreamteam/ObservableExample.java
+++ src/main/java/com/dreamteam/ObservableExample.java
@@ -1,5 +1,15 @@
 package com.dreamteam;
 
+import com.dreamteam.view.MainForm;
+import com.dreamteam.view.StatusColumnCellRenderer;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.text.TableView;
+import java.awt.*;
+
 public class ObservableExample {
     public static void main(String[] args) {
         Observer observer = new Observer();
@@ -7,5 +17,46 @@ public class ObservableExample {
 
         observable.addPropertyChangeListener(observer);
         observable.doThings();
+
+        JFrame frame = new JFrame();
+        var form  = new MainForm();
+        frame.setContentPane(form.getRootPanel());
+        frame.pack();
+        frame.setVisible(true);
+
+        DefaultTableModel model = (DefaultTableModel) form.getTable1().getModel();
+        model.addColumn("Column 1");
+        model.addColumn("Column 2");
+        model.addColumn("Column 3");
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+
+        TableColumn column = form.getTable1().getColumnModel().getColumn(0);
+
+        DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
+        renderer.setBackground(Color.blue);
+        renderer.setForeground(Color.yellow);
+        column.setCellRenderer(renderer);
+
+        //var cell = form.getTable1();
+       // DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
+      //  renderer.setBackground(Color.blue);
+      //  renderer.setForeground(Color.yellow);
+       // form.getTable1().getModel();
+        form.getTable1().getColumnModel().getColumn(1).setCellRenderer(new StatusColumnCellRenderer(2,2));
+
     }
 }
--- src/main/java/com/dreamteam/view/MainForm.java
+++ src/main/java/com/dreamteam/view/MainForm.java
@@ -1,6 +1,8 @@
 package com.dreamteam.view;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
 
 public class MainForm {
     private JPanel panel1;
@@ -10,4 +12,21 @@ public class MainForm {
     private JComboBox comboBox1;
     private JButton startButton;
     private JButton stopButton;
+
+    public JPanel getRootPanel()
+    {
+        return this.panel1;
+    }
+
+    public JTable getTable1()
+    {
+        return table1;
+    }
+
+
+    public void hello()
+    {
+        this.textField1.setText("olala");
+    }
 }
+
