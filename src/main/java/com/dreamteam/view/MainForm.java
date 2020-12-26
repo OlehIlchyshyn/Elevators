8ff2e51213656e10cd3ebb58a84a6aa561e481ce
Vladyslav Pryshchepa <vlad.prishchepa@gmail.com>
2020-12-26 22:12:53 +0200

    Added form

--- src/main/java/com/dreamteam/view/MainForm.java
+++ src/main/java/com/dreamteam/view/MainForm.java
@@ -1,4 +1,13 @@
 package com.dreamteam.view;
 
+import javax.swing.*;
+
 public class MainForm {
+    private JPanel panel1;
+    private JTable table1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JButton startButton;
+    private JButton stopButton;
 }
