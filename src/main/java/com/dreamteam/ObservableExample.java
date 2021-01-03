eb43d39ecefc5335903ffb503b0200f7d9f45ed2
Vladyslav Pryshchepa <vlad.prishchepa@gmail.com>
2021-01-04 01:13:24 +0200

    Added simple logic

--- src/main/java/com/dreamteam/ObservableExample.java
+++ src/main/java/com/dreamteam/ObservableExample.java
@@ -1,12 +1,18 @@
 package com.dreamteam;
 
+import com.dreamteam.view.ElevatorStatus;
 import com.dreamteam.view.MainForm;
 import com.dreamteam.view.ElevatorRenderer;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+import java.awt.*;
 
 public class ObservableExample {
+    public static int floorAmount = 10;
+    public static int elevatorAmount = 3;
+
     public static void main(String[] args) {
         Observer observer = new Observer();
         Observable observable = new Observable();
@@ -21,27 +27,28 @@ public class ObservableExample {
         frame.setVisible(true);
 
         DefaultTableModel model = (DefaultTableModel) form.getTable1().getModel();
-        model.addColumn("Column 1");
-        model.addColumn("Column 2");
-        model.addColumn("Column 3");
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-        model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
 
+        model.addColumn("Floor");
 
-        form.getTable1().getColumnModel().getColumn(1).setCellRenderer(new ElevatorRenderer(6));
-        form.getTable1().getColumnModel().getColumn(0).setCellRenderer(new ElevatorRenderer(3));
+        for(int i = 0; i < elevatorAmount; i++) {
+            model.addColumn("Elevator #" + i);
+        }
+
+        for(int i = 0; i < floorAmount; i++) {
+            model.addRow(new Object[elevatorAmount]);
+            model.setValueAt(floorAmount - i, i, 0);
+        }
+
+        form.getTable1().setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        form.getTable1().getColumnModel().getColumn(0).setMaxWidth(50);
+
+        ChangeCellColor(form.getTable1().getColumnModel(), 1, 2);
+        ChangeCellColor(form.getTable1().getColumnModel(), 2, 5);
+        ChangeCellColor(form.getTable1().getColumnModel(), 3, 8);
+    }
+
+    public static void ChangeCellColor(TableColumnModel model, int elevatorIndex, int floorIndex) {
+        model.getColumn(elevatorIndex)
+                .setCellRenderer(new ElevatorRenderer(floorAmount - floorIndex, ElevatorStatus.FULL));
     }
 }
--- src/main/java/com/dreamteam/view/ElevatorRenderer.java
+++ src/main/java/com/dreamteam/view/ElevatorRenderer.java
@@ -5,24 +5,37 @@ import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
 
 public class ElevatorRenderer extends DefaultTableCellRenderer {
-    private int _row;
-    public ElevatorRenderer(int row)
+    private int _floor;
+    private ElevatorStatus _status;
+
+    public ElevatorRenderer(int floor, ElevatorStatus status)
     {
-        this._row = row;
+        this._floor = floor;
+        this._status = status;
     }
+
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
     {
         Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 
-        // Only for specific cell
-        if (row == _row ) {
-            //c.setForeground(/*special foreground color*/);
-            c.setBackground(Color.BLUE);
+        c.setBackground(Color.WHITE);
+
+        if (row == _floor ) {
+            switch(_status) {
+                case FREE:
+                    c.setBackground(Color.GREEN);
+                    break;
+                case WORK:
+                    c.setBackground(Color.ORANGE);
+                    break;
+                case FULL:
+                    c.setBackground(Color.RED);
+                    break;
+                default:
+                    c.setBackground(Color.WHITE);
+            }
         }
-        else
-        {
-            c.setBackground(Color.WHITE);
-        }
+
         return c;
     }
 }
